3bb446a327ac46bf769c0c88ff9884ae98cf8501
wyqydsyq <wyqydsyq@gmail.com>
2026-02-19 18:22:39 +1000

    - Add option for MergeEntityListRef to skip deduplication

--- scripts/Game/EntityCatalog/DL_SCR_EntityCatalog.c
+++ scripts/Game/EntityCatalog/DL_SCR_EntityCatalog.c
@@ -2,19 +2,25 @@
 modded class SCR_EntityCatalog
 {
 	// copy provided entity list into local entity list, omitting any resources already processed by loot system
-	int MergeEntityListRef(notnull array<ref SCR_EntityCatalogEntry> entityList, EEntityCatalogType catalogType, FactionKey faction)
+	int MergeEntityListRef(notnull array<ref SCR_EntityCatalogEntry> entityList, EEntityCatalogType catalogType, FactionKey faction, bool checkDuplicates = true)
 	{
 		DL_LootSystem lootSystem = DL_LootSystem.GetInstance();
 		foreach (ref SCR_EntityCatalogEntry entityEntry : entityList)
 		{
-			if (!entityEntry || !entityEntry.GetPrefab() || lootSystem.processedResources.Contains(entityEntry.GetPrefab()))
+			if (
+				!entityEntry
+				|| !entityEntry.GetPrefab()
+				|| (checkDuplicates && lootSystem.processedResources.Contains(entityEntry.GetPrefab()))
+			)
 				continue;
 			
 			SCR_ArsenalItem item = SCR_ArsenalItem.Cast(entityEntry.GetEntityDataOfType(SCR_ArsenalItem));
 			if ((!item || !item.IsEnabled()) && catalogType != EEntityCatalogType.VEHICLE)
 				continue;
 			
-			lootSystem.processedResources.Insert(entityEntry.GetPrefab(), faction);
+			if (checkDuplicates)
+				lootSystem.processedResources.Insert(entityEntry.GetPrefab(), faction);
+			
 			m_aEntityEntryList.Insert(entityEntry);
 			m_mPrefabIndexes.Insert(entityEntry.GetPrefab(), m_aEntityEntryList.Find(entityEntry));
 		}
